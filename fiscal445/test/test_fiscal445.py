35a408cc086fb205d832a3105b8c0db0d5442c85
dv8edroute <dv8edroute@protonmail.com>
2020-03-03 19:31:45 -0500

    Update test_fiscal445.py

--- fiscal445/test/test_fiscal445.py
+++ fiscal445/test/test_fiscal445.py
@@ -5,19 +5,19 @@ import datetime
 class TestFiscal445(unittest.TestCase):
     
     def test_month_lookup(self):
-        self.assertEquals(fc5.month_lookup(10),'March')
-        self.assertEquals(fc5.month_lookup(10,3),'Mar')
+        self.assertEqual(fc5.month_lookup(10),'March')
+        self.assertEqual(fc5.month_lookup(10,3),'Mar')
         
     def test_week_lookup(self):
-        self.assertEquals(fc5.week_lookup(9),'Wk1')
-        self.assertEquals(fc5.week_lookup(10),'Wk2')
-        self.assertEquals(fc5.week_lookup(11),'Wk3')
-        self.assertEquals(fc5.week_lookup(12),'Wk4')
-        self.assertEquals(fc5.week_lookup(13),'Wk5')
+        self.assertEqual(fc5.week_lookup(9),'Wk1')
+        self.assertEqual(fc5.week_lookup(10),'Wk2')
+        self.assertEqual(fc5.week_lookup(11),'Wk3')
+        self.assertEqual(fc5.week_lookup(12),'Wk4')
+        self.assertEqual(fc5.week_lookup(13),'Wk5')
     
     def test_week_current(self):
         current = datetime.datetime.now().isocalendar()[1]
-        self.assertEquals(fc5.week_current(),current)
+        self.assertEqual(fc5.week_current(),current)
         
 
 
