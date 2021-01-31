7d0f252b3c86e31ace9e76e24403b5a066cb5943
dv8edroute <dv8edroute@protonmail.com>
2021-01-31 17:49:36 -0500

    fixed month to date

---
+++ fiscal445.0.1.6/fiscal445/__main__.py
@@ -0,0 +1,8 @@
+import fiscal445 as fc5
+
+import sys
+print('Use: python -m fiscal445 --help for more')
+if len(sys.argv)==2 and sys.argv[1]=='--help':
+    help(fc5)
+    help(fc5.Calendar)
+    help(fc5.Date_functions)
