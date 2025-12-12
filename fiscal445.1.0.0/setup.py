65320ea7c821a63fba049cc34990c26e647a92d9
Dv8edRoute <dv8edroute@protonmail.com>
2025-12-11 20:12:24 -0500

    Clean up __init__.py, add __version__, improve module docstring, added pyproject.toml, modified setup.py

--- fiscal445.1.0.0/setup.py
+++ fiscal445.1.0.0/setup.py
@@ -1,29 +1,6 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+if __name__ == "__main__":
+    setup()
 
-setuptools.setup(
-    name="fiscal445", 
-    version="1.0.0",
-    author="Dv8edRoute",
-    author_email="dv8edroute@protonmail.com",
-    license="MIT",
-    description="445 Fiscal calendar ",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/dv8edroute/fiscal445/tree/master/fiscal445.0.1.6",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Office/Business :: Financial :: Accounting",
-    ],
-    python_requires='>=3.6',
-    install_requires=[
-       "pandas>=0.25.3",
-       "numpy"
-   ],
-)
 
